6d97ec9e1aeccc930763afe7cf5bac2f119ce139
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 10:33:13 +0200

    Create interface IApiRequest for the classes with API requesting functionality

---
+++ CountriesService/ApiDataAccess/IApiRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace CountriesService.ApiDataAccess
+{
+    /// <summary>
+    /// Defines neccesary methods for classes with API requesting functionality.
+    /// </summary>
+    public interface IApiRequest
+    {
+        /// <summary>
+        /// Defines asynchronous method to retreive data in JSON format from the API.
+        /// </summary>
+        /// <param name="apiEndpoint">Targeted resource in the API to retreive data from.</param>
+        /// <returns>Raw data in JSON format.</returns>
+        public Task<string> GetJsonFromApiAsync(string apiEndpoint);
+    }
+}
