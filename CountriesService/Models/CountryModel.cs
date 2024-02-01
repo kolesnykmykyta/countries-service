c9e199871cc825afc9f2563b27a9ff2d65ebca65
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 09:47:21 +0200

    Add comments to the CountryModel

--- CountriesService/Models/CountryModel.cs
+++ CountriesService/Models/CountryModel.cs
@@ -7,15 +7,49 @@ using System.Threading.Tasks;
 
 namespace CountriesService.Models
 {
+    /// <summary>
+    /// Defines class to save the most common information about the country instance.
+    /// </summary>
     public class CountryModel
     {
+        /// <summary>
+        /// Common name of the country.
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Information about the local currency.
+        /// </summary>
         public CurrencyModel? Currency { get; set; }
+
+        /// <summary>
+        /// Capital of the country.
+        /// </summary>
         public string? Capital { get; set; }
+
+        /// <summary>
+        /// Region of the country.
+        /// </summary>
         public string? Region { get; set; }
+
+        /// <summary>
+        /// List of the official languages in the country.
+        /// </summary>
         public List<string>? Languages { get; set; }
+
+        /// <summary>
+        /// Area of the country.
+        /// </summary>
         public double Area { get; set; }
+
+        /// <summary>
+        /// Population of the country.
+        /// </summary>
         public int Population { get; set; }
+
+        /// <summary>
+        /// URL to the image of the country flag.
+        /// </summary>
         public string? FlagURL { get; set; }
     }
 }
