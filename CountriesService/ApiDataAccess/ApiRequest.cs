7a536ad8e5bf31ccfeaf4ef51e6914981ca45461
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 10:50:49 +0200

    Create class ApiRequest

---
+++ CountriesService/ApiDataAccess/ApiRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CountriesService.ApiDataAccess
+{
+    public class ApiRequest: IApiRequest
+    {
+        public async Task<string> GetJsonFromApiAsync(string apiEndpoint)
+        {
+            string output;
+
+            using (HttpClient client = new HttpClient())
+            {
+                output = await client.GetStringAsync(apiEndpoint);
+            }
+
+            return output;
+        }
+    }
+}
