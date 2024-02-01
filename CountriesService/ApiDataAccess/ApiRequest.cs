bc81bff77320eac6657931bab99c5d443ba20c0b
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 11:00:32 +0200

    Add comments to the ApiRequest class

--- CountriesService/ApiDataAccess/ApiRequest.cs
+++ CountriesService/ApiDataAccess/ApiRequest.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 
 namespace CountriesService.ApiDataAccess
 {
+    /// <summary>
+    /// Class that provides functionality to receive data from the API.
+    /// </summary>
     public class ApiRequest: IApiRequest
     {
+        /// <summary>
+        /// Method to retreive data from the API asynchronously.
+        /// </summary>
+        /// <param name="apiEndpoint">Targeted resource in the API to retreive data from.</param>
+        /// <returns>Raw data in JSON format.</returns>
         public async Task<string> GetJsonFromApiAsync(string apiEndpoint)
         {
             string output;
