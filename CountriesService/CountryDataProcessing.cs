d1047cc6b9ff2e962627069fa12837b2b5114f71
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 17:17:40 +0200

    Create CountryDataProcessing class and define its structure

---
+++ CountriesService/CountryDataProcessing.cs
@@ -0,0 +1,41 @@
+﻿using CountriesService.ApiDataAccess;
+using CountriesService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CountriesService
+{
+    public class CountryDataProcessing
+    {
+        private readonly IApiRequest _apirequester;
+
+        public CountryDataProcessing(IApiRequest apirequester)
+        {
+            _apirequester = apirequester;
+        }
+
+        public CountryModel GetCountryInfoByCode(string countryCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CountryModel GetCountryInfoByName(string countryName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public CountryModel GetCountryInfoByCapital(string capitalName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static CountryModel ConvertJsonIntoCountryModel(JsonDocument json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
