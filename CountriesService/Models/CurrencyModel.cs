8bc679d3888647ec78d2df6ee93118f74f90510a
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-01 09:49:08 +0200

    Add comments to the CurrencyModel

--- CountriesService/Models/CurrencyModel.cs
+++ CountriesService/Models/CurrencyModel.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace CountriesService.Models
 {
+    /// <summary>
+    /// Defines class to save the most common information about the currency.
+    /// </summary>
     public class CurrencyModel
     {
+        /// <summary>
+        /// Code of the currency.
+        /// </summary>
         public string? Code { get; set; }
+
+        /// <summary>
+        /// Name of the currency.
+        /// </summary>
         public string? Name { get; set; }
+
+        /// <summary>
+        /// Symbol of the currency.
+        /// </summary>
         public string? Symbol { get; set; }
     }
 }
