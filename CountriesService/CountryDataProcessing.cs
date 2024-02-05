645159fd904acf6718a687963f6d7b4217ad87de
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-02-05 17:49:17 +0200

    Add comments to the CountryDataProcessing class

--- CountriesService/CountryDataProcessing.cs
+++ CountriesService/CountryDataProcessing.cs
@@ -9,16 +9,32 @@ using System.Threading.Tasks;
 
 namespace CountriesService
 {
+    /// <summary>
+    /// Contains set of methods to get the country data from API by code, capital or name.
+    /// </summary>
     public class CountryDataProcessing
     {
+        /// <summary>
+        /// Instance of IApiRequest class that will handle API requests.
+        /// </summary>
         private readonly IApiRequest _apirequester;
         const string targetedApi = "https://restcountries.com/v3.1/";
 
+        /// <summary>
+        /// Initializes class with API requester.
+        /// </summary>
+        /// <param name="apirequester">Instance of IApiRequest class that will handle API requests.</param>
         public CountryDataProcessing(IApiRequest apirequester)
         {
             _apirequester = apirequester;
         }
 
+        /// <summary>
+        /// Provides information about the country by cca2, cca3, ccn3 or cioc country code.
+        /// </summary>
+        /// <param name="countryCode">cca2, cca3, ccn3 or cioc code of the country.</param>
+        /// <returns>CountryModel with information about the country.</returns>
+        /// <exception cref="ArgumentException">Thrown if country code is invalid, null, empty or whitespace.</exception>
         public async Task<CountryModel> GetCountryInfoByCodeAsync(string? countryCode)
         {
             if (string.IsNullOrWhiteSpace(countryCode))
@@ -40,6 +56,12 @@ namespace CountriesService
             }
         }
 
+        /// <summary>
+        /// Provides information about the country by common or official name.
+        /// </summary>
+        /// <param name="countryName">Common or official name.</param>
+        /// <returns>CountryModel with information about the country.</returns>
+        /// <exception cref="ArgumentException">Thrown if country name is invalid, null, empty or whitespace.</exception>
         public async Task<CountryModel> GetCountryInfoByNameAsync(string countryName)
         {
             if (string.IsNullOrWhiteSpace(countryName))
@@ -61,6 +83,12 @@ namespace CountriesService
             }
         }
 
+        /// <summary>
+        /// Provides information about the country by capital.
+        /// </summary>
+        /// <param name="capitalName">Capital of the country.</param>
+        /// <returns>CountryModel with information about the country.</returns>
+        /// <exception cref="ArgumentException">Thrown if capital is invalid, null, empty or whitespace.</exception>
         public async Task<CountryModel> GetCountryInfoByCapitalAsync(string capitalName)
         {
             if (string.IsNullOrWhiteSpace(capitalName))
@@ -81,6 +109,12 @@ namespace CountriesService
             }
         }
 
+        /// <summary>
+        /// Converts provided JSON form REST Countries v3.1 to the CountryModel object.
+        /// </summary>
+        /// <param name="json">JSON to convert</param>
+        /// <returns>CountryModel from specified JSON</returns>
+        /// <exception cref="JsonException">Thrown if any exceptions occured in the process of converting.</exception>
         private static async Task<CountryModel> ConvertJsonIntoCountryModelAsync(string json)
         {
             try
